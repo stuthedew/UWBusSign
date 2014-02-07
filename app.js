2ff500780a112420008a880b58ac2a9b2e5550fb
Stuart <Stuart@dyn-72-33-254-98.uwnet.wisc.edu>
2014-02-07 14:48:35 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,56 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+var RED = require("node-red");
+
+var databaseUrl = "data"; // "username:password@example.com/mydb"
+var collections = ["data"]
+var db = require("mongojs").connect(databaseUrl, collections);
+
+// New Code
+var mongo = require('mongodb');
+
+var app = express();
+app.locals.moment = require('moment');
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+var settings = {httpRoot:"/red", userDir:"node_modules/node-red", flowFile:"node_modules/node-red/flows_expresstest.json"};
+
+app.get('/', routes.index);
+app.get('/busTimes', routes.busTimes(db));
+
+var server = http.createServer(app);
+
+RED.init(server,settings);
+app.use(settings.httpRoot,RED.app);
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+RED.start();
+
